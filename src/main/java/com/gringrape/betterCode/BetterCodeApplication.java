d90081a6e39327c2577f63499c73a7d13f389b0d
gringrape <gringrape200@gmail.com>
2023-04-05 08:24:16 +0900

    Setup project
    Dependencies:
    - Spring Web
    - Spring Boot DevTools
    - Spring Data JPA
    - H2 Database
    
    Issue: https://github.com/gringrape/betterCode/issues/1

---
+++ src/main/java/com/gringrape/betterCode/BetterCodeApplication.java
@@ -0,0 +1,13 @@
+package com.gringrape.betterCode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gringrape/betterCode/BetterCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gringrape.betterCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
